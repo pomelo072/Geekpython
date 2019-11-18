b7adc294626489730140e64f3632259a5a1897bf
pomelo072 <54461099+pomelo072@users.noreply.github.com>
2019-11-18 20:26:43 +0800

    finish list

--- listprc.py
+++ listprc.py
@@ -1,7 +1,18 @@
+# list
 prac = ['hello', 'hi', 'goodbye']
-len(prac)
+print(len(prac))
 prac[1]
 prac[-1]
 prac.append('bye')
 prac.insert(2, 'nice')
 prac.pop(4)
+print(prac[3])
+# tuple
+prac2 = (1, 2)
+print(prac2[1])
+# dict
+prac3 = {'hello': 1, 'hi': 2, 'goodbye': 3}
+print(prac3['hi'])
+# set
+prac4 = ([1, 2, 5, 6, 8, 2, 4, 3, 5, 2, 6, 1])
+print(prac4)
