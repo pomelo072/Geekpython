0c087b42f57ba424c11536a20e4efda9b6ce1da0
pomelo072 <54461099+pomelo072@users.noreply.github.com>
2019-11-25 16:36:39 +0800

    edit day4

--- classprc.py
+++ classprc.py
@@ -17,3 +17,36 @@ pomelo = Student("lzx",98)
 print(pomelo.name)
 print(pomelo.score)
 print(pomelo.get_grade())
+
+class Animal(object):
+
+    def run(self):
+        print('Animal is running...')
+
+class Dog(Animal):
+
+    def run(self):
+        print('Dog is running...')
+
+class Cat(Animal):
+
+    def run(self):
+        print('Cat is running...')
+class people():
+
+    def run(self):
+        print("people do not extends Animal")
+
+def run_twice(animal):
+    animal.run()
+    animal.run()
+
+run_twice(Cat())
+run_twice(Animal())
+run_twice(people())
+
+print(type(Cat()))
+
+h = Cat()
+print(isinstance(h, Animal))
+print(dir(Animal))
